63ae1b19ff79373aca9b707c684a6ebb6eb0fdbb
ml3 <martin.laporte@cambridgeconsultants.com>
2022-02-08 18:49:21 +0000

    fix root missing, simplify code, format code, add comments

--- shell/ps1_shortener.py
+++ shell/ps1_shortener.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
+"""shortens the prompt path."""
 import os
 
+NB_DIGITS = 3
+
+# TODO: consider multiple users on 1 machine
+
 # INSTALLATION :
-install_prefix = "/usr/bin/"
+install_prefix = "/usr/bin/"  # must be in the PATH
 file_name = os.path.basename(__file__)
 if not os.path.isfile(install_prefix + file_name):
     from subprocess import call
-    # create link to location accessible from path (asks for root priviledges)
-    call(["sudo", "ln", "-fs",os.path.realpath(__file__), install_prefix])
-    # replace \w in the PS1 composition by a call to this script
-    call(["sed", "-i", f"/PS1=/ {{s/\\\\w/`{file_name}`/}}", f"{os.path.expanduser('~')}/.bashrc"])
 
-ps1 = os.getcwd().replace(os.path.expanduser("~"), "/~").split(os.sep)[1:]
-middle = [d[:3] for d in ps1[1:-1]]
-if len(ps1) > 1:
-    ps1 = os.path.join(ps1[0], *middle, ps1[-1])
-else:
-    ps1 = ps1[0]
+    # create link to location accessible from path (asks for root priviledges)
+    call(["sudo", "ln", "-fs", os.path.realpath(__file__), install_prefix])
+    # replace \w in the PS1 composition by a call to this script
+    call(
+        [
+            "sed",
+            "-i",
+            f"/PS1=/ {{s/\\\\w/`{file_name}`/}}",
+            f"{os.path.expanduser('~')}/.bashrc",
+        ]
+    )
+
+
+pwd = os.getcwd().replace(os.path.expanduser("~"), "/~")
+ps1 = pwd.split(os.sep)[1:]
+ps1[1:-1] = [d[:NB_DIGITS] for d in ps1[1:-1]]
+ps1 = os.path.join(*ps1)
+if ps1[0] != "~":
+    ps1 = "/" + ps1
 print(ps1)
